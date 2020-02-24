2b93a7447c3e09f7afdad2ffd6865813f5ea478c
spaliwal <spaliwal@adobe.com>
2020-02-24 14:46:01 +0530

    use action params to set env vars

--- dist/index.js
+++ dist/index.js
@@ -494,8 +494,16 @@ const execSync = __webpack_require__(129).execSync
 
 try {
   // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const namespace = core.getInput('AIO_RUNTIME_NAMESPACE');
+  console.log(`Namespace ${nameToGreet}!`);
+  process.env.AIO_RUNTIME_NAMESPACE = namespace
+  const auth = core.getInput('AIO_RUNTIME_AUTH');
+  if(auth) {
+    console.log(`auth present`);
+    process.env.AIO_RUNTIME_AUTH = auth
+  }
   // const time = (new Date()).toTimeString();
   // core.setOutput("time", time);
   // // Get the JSON webhook payload for the event that triggered the workflow
