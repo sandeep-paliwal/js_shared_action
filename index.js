5fd379fe79f6b733b4414cb6cdb9625793244521
spaliwal <spaliwal@adobe.com>
2020-02-24 15:10:04 +0530

    run deploy with set env

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ try {
       env: env
   }
 console.log("options " + util.inspect(options))
-   // child = execSync('sudo npm install -g @adobe/aio-cli', options)
-   // child = execSync('sudo aio -v', options)
-   // child = execSync('sudo aio app deploy', options)
+   child = execSync('sudo npm install -g @adobe/aio-cli', options)
+   child = execSync('sudo aio -v', options)
+   child = execSync('sudo aio app deploy', options)
 
 } catch (error) {
   core.setFailed(error.message);
