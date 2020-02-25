a6086ffa6e1f3fe7ff4cdaf3da1a5b06c4e1b1be
spaliwal <spaliwal@adobe.com>
2020-02-25 11:43:38 +0530

    fix env var issue

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const namespace = core.getInput('AIO_RUNTIME_NAMESPACE');
   console.log(`Namespace ${namespace}!`);
+  process.env.AIO_RUNTIME_NAMESPACE = namespace
   const auth = core.getInput('AIO_RUNTIME_AUTH');
   if(auth) {
     console.log(`auth present`);
+    process.env.AIO_RUNTIME_AUTH = auth
   }
   // const time = (new Date()).toTimeString();
   // core.setOutput("time", time);
@@ -23,13 +25,13 @@ try {
   }
   let options = {
       stdio: 'inherit',
-      env: env
+      env: process.env
   }
 console.log("options " + util.inspect(options))
-child = execSync('node test.js', options)
-   // child = execSync('sudo npm install -g @adobe/aio-cli', options)
-   // child = execSync('sudo aio -v', options)
-   // child = execSync('sudo aio app deploy', options)
+// child = execSync('node test.js', options)
+   child = execSync('sudo npm install -g @adobe/aio-cli', options)
+   child = execSync('sudo aio -v', options)
+   child = execSync('sudo aio app deploy', options)
 
 } catch (error) {
   core.setFailed(error.message);
